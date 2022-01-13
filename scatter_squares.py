3dd8c0c8732e91a1dfaddb6171a78da4c088af95
Lrving.xu <lrving@htek.com>
2022-01-13 17:14:25 +0800

    生成数据图

---
+++ scatter_squares.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 1001))
+y_values = [x ** 2 for x in x_values]
+
+plt.scatter(x_values, y_values, c='red', edgecolors='none', s=40)
+
+# 设置图表标题并给坐标轴指定标签等
+plt.title("Squares", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis="both", size=14)
+# 设置每个坐标轴的取值范围
+plt.axis([0, 1100, 1, 1100000])
+
+# 自动保存图表
+plt.savefig('squares_plot.png', bbox_inches='tight')
