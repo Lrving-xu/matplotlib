db4ce9b7e8f8a46c3e524bf8212cf3b22e306398
Lrving.xu <lrving@htek.com>
2022-01-14 14:28:11 +0800

    扔一次骰子生成的数据图

---
+++ die_visual.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from die import Die
+import pygal
+
+# 创建一个d6
+die = Die()
+
+# 扔几次骰子，并将结果存储在一个列表中
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+
+# 分析结果
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# 对结果可视化
+hist = pygal.Bar()
+
+hist.title = "1111"
+hist.x_lable = ['1','2','3','4','5','6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6',frequencies)
+hist.render_to_file('die_visual.svg')
